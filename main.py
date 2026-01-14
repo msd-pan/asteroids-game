2d0c6776dbf7e1f3effe4e792f38b140e9f51af2
xiaoka <panjixiaopan@gmail.com>
2026-01-14 13:26:10 +0900

    feat: restrict our game to draw a maximum of 60 times per second, or 60 FPS(frames per second)

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         log_state()
         for event in pygame.event.get():
@@ -15,6 +19,7 @@ def main():
                 return
         screen.fill('black')
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
