3a87db4f7561ccddf98ada3a00ec5ba214a24905
xiaoka <panjixiaopan@gmail.com>
2026-01-14 13:52:34 +0900

    feat: draw a spaceship for the player

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -18,8 +20,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill('black')
+        player.draw(screen)
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
+
 
 
 if __name__ == "__main__":
