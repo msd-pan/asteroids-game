209dc6ceeb73921f02543a4b667a53e2b526f0d0
xiaoka <panjixiaopan@gmail.com>
2026-01-15 10:05:30 +0900

    feat: make the asteroids disappear when they're destroyed

--- main.py
+++ main.py
@@ -44,6 +44,12 @@ def main():
                 log_event('player_hit')
                 print('Game over!')
                 sys.exit()
+
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    log_event('asteroid_shot')
+                    asteroid.kill()
+                    shot.kill()
                 
 
         screen.fill("black")
